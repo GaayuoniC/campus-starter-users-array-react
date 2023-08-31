e77a798a03849317b106d36a1129f59ac259ef7c
Christian Gaayuoni <gayuoni@proton.me>
2023-08-31 22:06:57 +0200

    Add styling to navigation tabs

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,13 +4,13 @@ export function Navigation() {
     <div>
       <nav>
         <div className="filter-by">
-          <p>Filter by</p>
-          <ul>All</ul>
+          <p>Filter by;</p>
+          <ul>All </ul>
           <ul>Men</ul>
           <ul>Women</ul>
         </div>
         <div className="sort-by">
-          <p>Sort by</p>
+          <p>Sort by;</p>
           <ul>Name</ul>
           <ul>Age</ul>
         </div>
